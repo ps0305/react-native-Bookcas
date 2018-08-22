023e5e23cfff5c17d8a2711fb64907c0b7258cb6
Pankaj Singh <singh.pankaj@live.com>
2018-08-22 17:16:23 +0530

    commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import App from './App.js';
+
+class Boookcase extends Component {
+  render() {
+    return (
+      
+        <App/>
+      
+    );
+  }
+}
